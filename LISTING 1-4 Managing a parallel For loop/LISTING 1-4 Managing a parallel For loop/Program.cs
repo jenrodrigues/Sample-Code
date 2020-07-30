113418ce8cd5507852ebf9980d62fee113745486
jenrodrigues <jen.mrodrigues@gmail.com>
2020-07-30 18:58:00 +0100

    Listing 1.4 with comments.

--- LISTING 1-4 Managing a parallel For loop/LISTING 1-4 Managing a parallel For loop/Program.cs
+++ LISTING 1-4 Managing a parallel For loop/LISTING 1-4 Managing a parallel For loop/Program.cs
@@ -19,17 +19,24 @@ namespace LISTING_1_4_Managing_a_parallel_For_loop
         {
             var items = Enumerable.Range(0, 500).ToArray();
 
+
+            //ParallelLoopResult type can be used on For() and ForEach() methods to determine 
+            //whether or not a parallel loop has successfully completed.
+            
+            //ParallelLoopState type allows the code to control the iteration process 
             ParallelLoopResult result = Parallel.For(0, items.Count(), (int i, ParallelLoopState loopState) =>
             {
                 if (i == 200)
-                    loopState.Break();
+                    loopState.Break(); //Small difference from Stop()
 
                 WorkOnItem(items[i]);
+                j++;
+
             });
 
-            Console.WriteLine("Completed: " + result.IsCompleted);
+            Console.WriteLine("Completed: " + result.IsCompleted);//checking the loop final result
             Console.WriteLine("Items: " + result.LowestBreakIteration);
-
+            
             Console.WriteLine("Finished processing. Press a key to end.");
             Console.ReadKey();
         }
